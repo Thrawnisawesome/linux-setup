23998ed55722b9de089fef8df4081101e16bc0a4
Thomas Harris <164784721+Thrawnisawesome@users.noreply.github.com>
2024-06-28 11:28:35 +0100

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install flatpak calc neofetch
+sudo apt install flatpak calc neofetch cmatrix nudoku
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install org.mozilla.firefox -y
+flatpak install org.mozilla.firefox com.visualstudio.code -y
